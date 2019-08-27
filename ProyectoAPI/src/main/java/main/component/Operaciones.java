99b76879a2b31bc52de25a9879318d276961b566
mvaliente <mvaliente@WS-MVALIENTE.dap.es>
2019-08-27 23:40:32 +0200

    Subida de Proyectos

---
+++ ProyectoAPI/src/main/java/main/component/Operaciones.java
@@ -0,0 +1,27 @@
+package main.component;
+
+import java.io.Serializable;
+/**
+ * The Interface Operaciones.
+ */
+public interface Operaciones extends Serializable {
+	
+	/**
+	 * Sumador.
+	 *
+	 * @param elemento1 the elemento 1
+	 * @param elemento2 the elemento 2
+	 * @return the double
+	 */
+	public Double sumador (Double elemento1,Double elemento2);
+	
+	/**
+	 * Restador.
+	 *
+	 * @param elemento1 the elemento 1
+	 * @param elemento2 the elemento 2
+	 * @return the double
+	 */
+	public Double restador (Double elemento1,Double elemento2);
+
+}
